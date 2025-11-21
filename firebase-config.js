235e35b376aad62c3887aa86fdb19861a83373ed
uzsunkris-crypto <smilealexsunday@gmail.com>
2025-11-21 03:59:49 +0100

    Update and rename about.html to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// Replace the object below with your Firebase client config from Firebase Console (Safe to be public)
+window.FIREBASE_CLIENT_CONFIG = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
