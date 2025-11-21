a2c5baa00dacca1734c35f3e93781e41269d6558
uzsunkris-crypto <smilealexsunday@gmail.com>
2025-11-21 13:38:41 +0100

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,9 @@
+{
+  "name": "Search",
+  "short_name": "Search",
+  "start_url": "/dashboard.html",
+  "display": "standalone",
+  "background_color": "#081018",
+  "theme_color": "#0AA83F",
+  "icons":[]
+}
